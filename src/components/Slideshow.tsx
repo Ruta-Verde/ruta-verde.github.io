8db585cd5cf95716f394ebef3dab97bb72a9eb50
root <root@DESKTOP-C1259TS>
2024-06-27 21:23:10 -0400

    sliding animation finally works

--- src/components/Slideshow.tsx
+++ src/components/Slideshow.tsx
@@ -1,5 +1,6 @@
 import { useState, useEffect } from 'react';
 import { Box, Image, Button, Flex, Fade } from '@chakra-ui/react';
+import '../styles/slideshow.css';
 
 interface SlideProps {
   images: string[];
@@ -7,27 +8,19 @@ interface SlideProps {
 
 function Slideshow({ images }: SlideProps) {
   const [currentImageIndex, setCurrentImageIndex] = useState(0);
-  const [fadeIn, setFadeIn] = useState(true);
+  // const [fadeIn, setFadeIn] = useState(true);
 
   // Function to handle dot clicks
   const handleDotClick = (index: number) => {
-    setFadeIn(false);
-    setTimeout(() => {
-      setCurrentImageIndex(index);
-      setFadeIn(true);
-    }, 600);
+    setCurrentImageIndex(index);
   };
 
   // Effect to automatically change slides
   useEffect(() => {
     const timer = setInterval(() => {
-      setFadeIn(false);
-      setTimeout(() => {
-        setCurrentImageIndex((prevIndex) =>
-          prevIndex === images.length - 1? 0 : prevIndex + 1
-        );
-        setFadeIn(true);
-      }, 600);
+      setCurrentImageIndex((prevIndex) =>
+        prevIndex === images.length - 1? 0 : prevIndex + 1
+      );
     }, 5000); // Change slide every 5 seconds
 
     return () => clearInterval(timer); // Cleanup on unmount
@@ -36,30 +29,32 @@ function Slideshow({ images }: SlideProps) {
   return (
     <Box position='relative' w='100%'>
     {/* Display current image */}
-    <Fade in={fadeIn}>
-      <Image
-        src={images[currentImageIndex]}
-        alt={`Slide ${currentImageIndex}`}
-        objectFit='cover'
-        w='100%'
-      />
-    </Fade>
-
-    {/* Navigation dots */}
-    <Flex justify='center' mt='10' position='absolute' bottom='30px' left='50%' transform='translateX(-50%)'>
-      {images.map((_, index) => (
-        <Button
-          key={index} onClick={() => handleDotClick(index)}
-          size='xs'
-          bg='white'
-          colorScheme='gray'
-          borderRadius='full'
-          border='1px solid'
-          opacity='70%'
-          _hover={{ opacity: '100%' }}
-          m={1} />
-      ))}
-    </Flex>
+      <Flex w='100%' h='100%' overflow='hidden'>
+        {images.map( url => (
+          <Image key={url} src={url} className='slider' style={{
+            translate: `${-100 * currentImageIndex}%`}} />
+        ))}
+        <Image
+          src={images[currentImageIndex]}
+          alt={`Slide ${currentImageIndex}`}
+          className='slider'
+        />
+      </Flex>
+      {/* Navigation dots */}
+      <Flex justify='center' mt='10' position='absolute' bottom='30px' left='50%' transform='translateX(-50%)'>
+        {images.map((_, index) => (
+          <Button
+            key={index} onClick={() => handleDotClick(index)}
+            size='xs'
+            bg='white'
+            colorScheme='gray'
+            borderRadius='full'
+            border='1px solid'
+            opacity='70%'
+            _hover={{ opacity: '100%' }}
+            m={1} />
+        ))}
+      </Flex>
   </Box>
   );
 };
