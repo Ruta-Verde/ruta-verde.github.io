18ce519cdf27232eda385306630241d1484751a1
Your Name <you@example.com>
2024-07-17 12:25:39 -0400

    Some changes

--- src/components/ContactForm.tsx
+++ src/components/ContactForm.tsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import '../styles/contactForm.css';
-import { Box, Button, FormControl, FormLabel, Input, Textarea, Heading } from '@chakra-ui/react';
+import { Box, Button, FormControl, FormLabel, FormHelperText, Input, Textarea, Heading } from '@chakra-ui/react';
 
 function ContactForm() {
   const [formState, setFormState] = useState({ name: '', email: '', message: '' });
@@ -18,25 +18,12 @@ function ContactForm() {
 
   return (
     <Box id="contact" py={10}>
-      <Heading as="h2" size="xl" mb={6}>
-        Contact Us
-      </Heading>
-      <form onSubmit={handleSubmit}>
-        <FormControl mb={4}>
-          <FormLabel>Name</FormLabel>
-          <Input type="text" name="name" value={formState.name} onChange={handleChange} placeholder="Name" required />
+      <form> {/* Use formik? */}
+        <FormControl>
+          <FormLabel>Contact Us!</FormLabel>
+          <Input type='email' placeholder='Email' />
+          <FormHelperText>Stay up to date with our upcoming events!</FormHelperText>
         </FormControl>
-        <FormControl mb={4}>
-          <FormLabel>Email</FormLabel>
-          <Input type="email" name="email" value={formState.email} onChange={handleChange} placeholder="Email" required />
-        </FormControl>
-        <FormControl mb={4}>
-          <FormLabel>Message</FormLabel>
-          <Textarea name="message" value={formState.message} onChange={handleChange} placeholder="Message" required />
-        </FormControl>
-        <Button type="submit" colorScheme="teal">
-          Send
-        </Button>
       </form>
     </Box>
   );
