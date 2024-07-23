03fac1e546ef3327528952f9004fc1f1e632d9ad
Your Name <you@example.com>
2024-07-22 22:26:58 -0400

    organization changes

--- src/components/Slideshow.tsx
+++ src/components/Slideshow.tsx
@@ -53,10 +53,10 @@ function Slideshow({ slides }: SlideProps) {
             translate: `${-100 * imageIndex}%`, filter: 'brightness(90%)'}}>
             <Image key={slide.image} src={slide.image} className='slider' />
             <Box position='absolute' top='30%' left='30%' textColor='white' textAlign='left'>
-              <Text fontSize='5xl' as='b'>
+              <Text fontSize={['xl', '2xl', '3xl', '4xl', '5xl']} as='b'>
                 {slide.title}
               </Text>
-              <Text fontSize='lg' mt={4} width='20rem'>
+              <Text fontSize={['xs', 'sm', 'md', 'lg', 'xl']} mt={4} width='20rem'>
                 {slide.text}
               </Text>
             </Box>
---
+++ src/pages/Blog.tsx
@@ -0,0 +1,7 @@
+// Likely want to do something with an image at the top, the author, then a preview of the post, then a click for more button at the bottom
+
+function Blog() {
+
+}
+
+export default Blog;
