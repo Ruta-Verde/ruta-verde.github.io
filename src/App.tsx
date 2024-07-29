dadb6533e1ec88e5187846680feefece347a06aa
Will <will@rutaverde.org>
2024-07-29 18:14:20 -0400

    attempt to remove app.tsx failures

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import Footer from './components/Footer.tsx';
 import Home from './pages/Home.tsx';
 import About from './pages/About.tsx';
 import Donate from './pages/Donate.tsx';
-import Events from './pages/Events.tsx';
+// import Events from './pages/Events.tsx';
 import { Box, ChakraProvider, Flex } from '@chakra-ui/react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import theme from './theme.tsx'
@@ -21,7 +21,7 @@ function App() {
                 <Route path="/" element={<Home />}/>
                 <Route path="/about" element={<About />} />
                 <Route path="donate" element={<Donate />}/>
-                <Route path="events" element={<Events />}/>
+{/*                 <Route path="events" element={<Events />}/> */}
             </Routes>
           </Flex>
           <Footer />
