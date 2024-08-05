9a81ffefe8918ffa8a254c0f8dba5d5148ad1dcd
Cameron Henderson <cameronhenderson11@hotmail.com>
2024-08-04 18:51:34 -0700

    fix build

--- src/components/EventsCarousel.tsx
+++ src/components/EventsCarousel.tsx
@@ -1,6 +1,5 @@
 // Likely want to do something with an image at the top, the author, then a preview of the post, then a click for more button at the bottom
 import {
-    Box,
     Flex,
     Text,
     Card,
