737b3e8678eb6c368fc2d355a6fb31af33f9be39
Cameron Henderson <cameronhenderson11@hotmail.com>
2024-08-24 08:34:32 -0700

    Display events as finished vs not finished, and chronologically

--- src/components/EventsCarousel.tsx
+++ src/components/EventsCarousel.tsx
@@ -11,13 +11,15 @@ import {
 import { Carousel } from './Carousel.tsx'
 import { CarouselProps } from './Carousel.tsx'
 import { eventList } from '../events_data/events.ts';
+import { VolunteerEvent as Event } from '../events_data/events.ts';
 import pic from '../assets/research.png';
 
 const dateOptions: Intl.DateTimeFormatOptions = { 
     weekday: "long",
     month: 'long',
     day: '2-digit',
-    year: 'numeric'
+    year: 'numeric',
+    timeZone: 'UTC',
 }
 
 const carouselProps: CarouselProps = {
@@ -26,6 +28,18 @@ const carouselProps: CarouselProps = {
     cardWidth: '250px',
 }
 
+function compareDate(a:Event,b:Event) {
+  if (a.date < b.date)
+     return -1;
+  if (a.date > b.date)
+    return 1;
+  return 0;
+}
+
+var upcomingEventList: Event[] = eventList
+    .filter(event => event.isFinished === false)
+    .sort(compareDate);
+
 function EventsCarousel() {
     return (
         <Flex 
@@ -33,114 +47,120 @@ function EventsCarousel() {
         alignItems='center' justifyContent='center' 
         bg='#F0F0F0'
         >
-            <Carousel carouselProps={carouselProps}>
-                {eventList.map( event => 
-                    <Card
-                    h='500px' w='250px' 
-                    scrollSnapAlign='center' 
-                    borderRadius='15px'
-                    bg='#FFF'
-                    flexShrink='0' 
-                    boxShadow='0px 4px 10px 0px rgba(0, 0, 0, 0.15)'
-                    overflow='hidden'
-                    >
-                        <Flex
-                        w='100%' h='200px'
+            {upcomingEventList.length === 0 ? (
+                <Text>
+                    No upcoming events at the moment
+                </Text>
+            ) : (
+                <Carousel carouselProps={carouselProps}>
+                    {upcomingEventList.map( event => 
+                        <Card
+                        h='500px' w='250px' 
+                        scrollSnapAlign='center' 
+                        borderRadius='15px'
+                        bg='#FFF'
+                        flexShrink='0' 
+                        boxShadow='0px 4px 10px 0px rgba(0, 0, 0, 0.15)'
+                        overflow='hidden'
                         >
-                            {/* corner date tag */}
-                            <Flex 
-                            flexDirection='column'
-                            padding='5px'
-                            position='absolute'
-                            w='70px' h='70px'
-                            bg='#E9D523'
-                            left='0'
-                            top='0'
-                            borderRadius='15px 0px 15px 0px'
-                            >
-                                <Text fontSize='15px' fontWeight='700'>
-                                    {event.date.toLocaleDateString('default', {month: "short" })}
-                                </Text>
-                                <Text marginTop='-15px' fontSize='40px' fontWeight='700'>
-                                    {event.date.toLocaleDateString('default', {day: "2-digit" })}
-                                </Text>
-                            </Flex>
-                            <Image
-                            objectFit='cover'
-                            minW='100%'
-                            alt="event"
-                            src={pic}/>
-                        </Flex>
-
-                        {/* card header and body text */}
-                        <Flex
-                        padding='20px'
-                        flexDirection='column' 
-                        flexGrow='1'
-                        alignItems='left' 
-                        textAlign='left' 
-                        overflowY='hidden'>
                             <Flex
-                            marginBottom='20px'
+                            w='100%' h='200px'
                             >
-                                <Heading 
-                                color='#385C40'
-                                fontSize='30px' 
-                                fontWeight='700' 
+                                {/* corner date tag */}
+                                <Flex 
+                                flexDirection='column'
+                                padding='5px'
+                                position='absolute'
+                                w='70px' h='70px'
+                                bg='#E9D523'
+                                left='0'
+                                top='0'
+                                borderRadius='15px 0px 15px 0px'
                                 >
-                                    {event.title} 
-                                </Heading>
+                                    <Text fontSize='15px' fontWeight='700'>
+                                        {event.date.toLocaleDateString('default', {month: "short", timeZone: 'UTC'})}
+                                    </Text>
+                                    <Text marginTop='-15px' fontSize='40px' fontWeight='700'>
+                                        {event.date.toLocaleDateString('default', {day: "2-digit", timeZone: 'UTC'})}
+                                    </Text>
+                                </Flex>
+                                <Image
+                                objectFit='cover'
+                                minW='100%'
+                                alt="event"
+                                src={pic}/>
                             </Flex>
 
-                            <Flex>
-                                <Text 
-                                color='#385C40'
-                                fontSize='15px'
-                                fontWeight='700'
+                            {/* card header and body text */}
+                            <Flex
+                            padding='20px'
+                            flexDirection='column' 
+                            flexGrow='1'
+                            alignItems='left' 
+                            textAlign='left' 
+                            overflowY='hidden'>
+                                <Flex
+                                marginBottom='20px'
                                 >
-                                    {event.date.toLocaleDateString('default', dateOptions)}
-                                </Text>
-                            </Flex>
-                            <Flex marginBottom='20px'>
-                                <Text 
-                                color='#385C40'
-                                fontSize='15px'
-                                fontWeight='400'
-                                textTransform='uppercase'
-                                >
-                                    {event.location}
-                                </Text>
-                            </Flex>
-                            <Flex flexGrow='1' overflow='hidden'>
-                                <Text 
-                                fontSize='12px'
-                                >
-                                    {event.about}
-                                </Text>
-                            </Flex>
-
-                            <Link
-                                href={'/#/events/' + event.slug} 
-                            >
-                                <Button 
-                                    minHeight='40px' w='100%'
-                                    marginTop='15px'
-                                    borderRadius='15px'
-                                    alignItems='center'
-                                    justifyContent='center'
-                                    bg='#E9D523'
+                                    <Heading 
+                                    color='#385C40'
+                                    fontSize='30px' 
+                                    fontWeight='700' 
                                     >
-                                        <Text 
-                                        fontSize='15px' 
-                                        fontWeight='600'>
-                                            More Information
-                                        </Text>
-                                </Button>
-                            </Link>
-                        </Flex>
-                    </Card>
-                )}
-            </Carousel>
+                                        {event.title} 
+                                    </Heading>
+                                </Flex>
+
+                                <Flex>
+                                    <Text 
+                                    color='#385C40'
+                                    fontSize='15px'
+                                    fontWeight='700'
+                                    >
+                                        {event.date.toLocaleDateString('default', dateOptions)}
+                                    </Text>
+                                </Flex>
+                                <Flex marginBottom='20px'>
+                                    <Text 
+                                    color='#385C40'
+                                    fontSize='15px'
+                                    fontWeight='400'
+                                    textTransform='uppercase'
+                                    >
+                                        {event.location}
+                                    </Text>
+                                </Flex>
+                                <Flex flexGrow='1' overflow='hidden'>
+                                    <Text 
+                                    fontSize='12px'
+                                    >
+                                        {event.about}
+                                    </Text>
+                                </Flex>
+
+                                <Link
+                                    href={'/#/events/' + event.slug} 
+                                >
+                                    <Button 
+                                        minHeight='40px' w='100%'
+                                        marginTop='15px'
+                                        borderRadius='15px'
+                                        alignItems='center'
+                                        justifyContent='center'
+                                        bg='#E9D523'
+                                        >
+                                            <Text 
+                                            fontSize='15px' 
+                                            fontWeight='600'>
+                                                More Information
+                                            </Text>
+                                    </Button>
+                                </Link>
+                            </Flex>
+                        </Card>
+                    )}
+                </Carousel>
+            )}
         </Flex>
     )
 }
