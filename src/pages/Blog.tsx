0d0edc0c60f545d649aec794121aa83d639734ca
Your Name <you@example.com>
2024-08-26 14:54:42 -0400

    page additions to blog

--- src/pages/Blog.tsx
+++ src/pages/Blog.tsx
@@ -1,17 +1,42 @@
 import blogheader from '../assets/blogheader.jpg';
-import {Box, Image, Text} from '@chakra-ui/react';
+import {
+  Box,
+  Flex,
+  Image,
+  Text,
+  Heading,
+  // Card,
+  // CardBody,
+  // CardFooter
+} from '@chakra-ui/react';
 
 function Blog() {
   return (
     <Box w='100%'>
-      <Box>
+      <Box h='100px' position='relative'>
         <Image position='absolute' src={blogheader} h='100px' w='100%' objectFit='cover' zIndex='0'/>
         <Box position='relative' h='100px' w='100%' bgGradient='linear(to-r, rgba(47, 71, 53, 0.8), rgba(7, 19, 25, 0))' />
-        <Text position='absolute' mt={['-20%']} ml='30%' textColor='white' fontSize='xl' fontWeight='bold' textAlign='center'>Ruta Verde Blog</Text>
+        <Text position='absolute' left='0' right='0' top='50px' bottom='0' m='auto' w='100%' h='100px' textColor='white' fontSize='3xl' fontWeight='bold'>Ruta Verde Blog</Text>
       </Box>
-      {/* <Heading /> */}
+      <Flex h='80px' alignItems='center' justifyContent='center'>
+        <Heading position='relative' textColor='#E9D523' fontSize='2xl' fontWeight='bold'>Latest From Ruta Verde</Heading>
+      </Flex>
     </Box>
   )
 }
+
+// function BlogCard() {
+
+// }
+
+// function MainCard() {
+//   return(
+//     <>
+//       <Card direction='row' overflow='hidden'>
+
+//       </Card>
+//     </>
+//   ) 
+// }
 
 export default Blog;
