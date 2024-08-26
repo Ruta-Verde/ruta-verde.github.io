4a7d09a00fdde343d7ad78087de87fd9b99ba2e4
Your Name <you@example.com>
2024-08-26 10:04:07 -0400

    starter blog

--- src/pages/Blog.tsx
+++ src/pages/Blog.tsx
@@ -1,7 +1,16 @@
+import blogheader from '../assets/blogheader.jpg';
+import {Box, Image, Text} from '@chakra-ui/react';
+
 function Blog() {
   return (
-    <>
-    </>
+    <Box w='100%'>
+      <Box>
+        <Image position='absolute' src={blogheader} h='100px' w='100%' objectFit='cover' zIndex='0'/>
+        <Box position='relative' h='100px' w='100%' bgGradient='linear(to-r, rgba(47, 71, 53, 0.8), rgba(7, 19, 25, 0))' />
+        <Text position='absolute' mt={['-20%']} ml='30%' textColor='white' fontSize='xl' fontWeight='bold' textAlign='center'>Ruta Verde Blog</Text>
+      </Box>
+      {/* <Heading /> */}
+    </Box>
   )
 }
 
