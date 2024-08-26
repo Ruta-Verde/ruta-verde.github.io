bd1c9f7c8e2108c6169178a8bab273100fbfe126
Your Name <you@example.com>
2024-08-26 16:31:36 -0400

    blog main page updates, changed footer

--- src/blog_data/blogs.ts
+++ src/blog_data/blogs.ts
@@ -1,12 +1,14 @@
 import file1 from '../assets/magic.pdf';
 
-type BlogInfo = {
+export type BlogInfo = {
   slug: string;
   author: string;
+  title: string;
+  summary: string;
   date: Date;
   src: string;
 }
 
 export const blogList: BlogInfo[] = [
-  {slug: 'test', author: 'Joao Vilca Soto', date: new Date(), src: file1}
+  {slug: 'test', author: 'Joao Vilca Soto', title: 'Testing', summary: 'Testing a thing out', date: new Date(), src: file1}
 ]
