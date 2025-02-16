619290d31b2743ba9f4dbe263e1dc0a90acfc71e
William Ceriale <will@rutaverde.org>
2025-02-16 11:15:26 +0100

    update donation link (#51)

--- src/pages/GetInvolved.tsx
+++ src/pages/GetInvolved.tsx
@@ -17,7 +17,7 @@ const cards:GetInvolvedInfo[] = [
   {header: 'Donate', 
    text: getInvolvedDonate,
    imageUrl: 'https://www.lismore.nsw.gov.au/files/assets/public/v/1/1.-households/4.-pets-amp-animals/images/kitten.jpg?dimension=pageimage&w=480', 
-   buttonUrl: 'https://www.iatspayments.com/saaura/PA9ACE27D2159C5D04?fbclid=IwAR20hCOzgY97-Gg5nX5oTeQT62TfWgwECUHjDR6wVAgixHHwxQR898UdPZY',
+   buttonUrl: 'https://donate.stripe.com/bIYcNY7yY9whe2I4gg',
    isButtonExternal: true,
   },
   {header: 'Internships', 
@@ -45,6 +45,6 @@ function GetInvolved() {
         )}
       </SimpleGrid>
   );
-};
+}
 
 export default GetInvolved;
