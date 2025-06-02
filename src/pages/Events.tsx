a0b6ffaa7435bba46ca928938fc78b97045bda78
Will <will@rutaverde.org>
2025-06-02 11:59:58 -0400

    update events page. clean up banner in other pages

--- src/pages/Events.tsx
+++ src/pages/Events.tsx
@@ -14,6 +14,8 @@ import { eventList } from '../events_data/events.ts';
 import { VolunteerEvent as Event } from '../events_data/events.ts';
 import yellowRightArrow from '../assets/YellowRightArrow.svg';
 import { useLayoutEffect } from 'react';
+import Linkify from 'linkify-react';
+import blogheader from '../assets/blogheader.jpg';
 
 function compareDate(a:Event,b:Event) {
   if (a.date < b.date)
@@ -23,7 +25,7 @@ function compareDate(a:Event,b:Event) {
   return 0;
 }
 
-var pastEventList: Event[] = eventList
+let pastEventList: Event[] = eventList
     .filter(event => event.isFinished === true)
     .sort(compareDate);
 pastEventList = pastEventList.slice(-5);
@@ -34,105 +36,101 @@ export function Events() {
     });
     
     return (
-        <Box w='100vw'>
-            <Box
-            w='100%' h={{base: '100px', sm: '150px'}} 
-            bg='#385C40' 
-            textAlign='center' 
-            fontSize={{base: '35px', sm: '50px'}}
-            textColor='white'
-            fontWeight='600'
-            alignContent='center'
-            fontFamily='josefinSans'>
-                Upcoming Events
-            </Box>
-            <EventsCarousel />
-            <Flex 
-            id='past-events'
-            w='100vw'
-            justifyContent='center'
-            bg='#143343'
-            >
-                <Flex
-                flexDir='column'
-                w='850px'
-                marginTop='60px'
-                marginBottom='90px'
-                marginX='30px'
-                alignItems='center'
-                textColor='white'
+        <Linkify>
+            <Box w='100vw'>
+                    <Box h='100px' position='relative'>
+                    <Image position='absolute' src={blogheader} h='100px' w='100%' objectFit='cover' zIndex='0'/>
+                    <Box position='relative' h='100px' w='100%' bgGradient='linear(to-r, rgba(47, 71, 53, 0.8), rgba(7, 19, 25, 0))' />
+                    <Text position='absolute' left='0' right='0' top='50px' bottom='0' m='auto' w='100%' h='100px' textColor='white' fontSize='3xl' fontWeight='bold'>Upcoming Events</Text>
+                    </Box>
+                <EventsCarousel />
+                <Flex 
+                id='past-events'
+                w='100vw'
+                justifyContent='center'
+                bg='#143343'
                 >
-                    <Heading
-                    color='white'
-                    fontSize='53px'
-                    fontWeight='800'
-                    marginBottom='30px'
-                    textTransform='uppercase'>
-                        Past Events
-                    </Heading>
-                    {pastEventList.length === 0 ? (
-                        <Box w='100%'>
-                            <Box h='2px' w='100%' bg='white'></Box>
-                            <Text mt='40px'>
-                                No events
-                            </Text>
-                        </Box>
-                    ) : (
-                        <Flex flexDir='column' w='100%'>
-                            {pastEventList.map( event =>
-                                <LinkBox
-                                as={RouterLink}
-                                to={'/events/' + event.slug} 
-                                display='flex'
-                                id='event-row'
-                                flexDir='column'
-                                w='100%'
-                                >
-                                    <Box h='2px' w='100%' bg='white'></Box>
-                                    <Flex paddingY='22px'>
-                                        <Box w='10px' h='100%' bg='yellow'> </Box>
-                                        <Flex w='min(80px, 5%)'></Flex>
-                                        <Flex
-                                        id='event-date'
-                                        flexDir='column'
-                                        justifyContent='center'
-                                        alignItems='center'
-                                        >
-                                            <Text fontSize={{base: '23px', sm: '30px'}} fontWeight='700'>
-                                                {event.date.toLocaleDateString('default', {month: "short", timeZone: 'UTC'})}
-                                            </Text>
-                                            <Text marginTop='-15px' fontSize={{base: '45px', sm: '60px'}} fontWeight='700'>
-                                                {event.date.toLocaleDateString('default', {day: "2-digit", timeZone: 'UTC'})}
-                                            </Text>
-                                        </Flex>
-                                        <Flex w='min(150px, 10%)'></Flex>
+                    <Flex
+                    flexDir='column'
+                    w='850px'
+                    marginTop='60px'
+                    marginBottom='90px'
+                    marginX='30px'
+                    alignItems='center'
+                    textColor='white'
+                    >
+                        <Heading
+                        color='white'
+                        fontSize='53px'
+                        fontWeight='800'
+                        marginBottom='30px'
+                        textTransform='uppercase'>
+                            Past Events
+                        </Heading>
+                        {pastEventList.length === 0 ? (
+                            <Box w='100%'>
+                                <Box h='2px' w='100%' bg='white'></Box>
+                                <Text mt='40px'>
+                                    No events
+                                </Text>
+                            </Box>
+                        ) : (
+                            <Flex flexDir='column' w='100%'>
+                                {pastEventList.map( event =>
+                                    <LinkBox
+                                    as={RouterLink}
+                                    to={'/events/' + event.slug} 
+                                    display='flex'
+                                    id='event-row'
+                                    flexDir='column'
+                                    w='100%'
+                                    >
+                                        <Box h='2px' w='100%' bg='white'></Box>
+                                        <Flex paddingY='22px'>
+                                            <Box w='10px' h='100%' bg='yellow'> </Box>
+                                            <Flex w='min(80px, 5%)'></Flex>
+                                            <Flex
+                                            id='event-date'
+                                            flexDir='column'
+                                            justifyContent='center'
+                                            alignItems='center'
+                                            >
+                                                <Text fontSize={{base: '23px', sm: '30px'}} fontWeight='700'>
+                                                    {event.date.toLocaleDateString('default', {month: "short", timeZone: 'UTC'})}
+                                                </Text>
+                                                <Text marginTop='-15px' fontSize={{base: '45px', sm: '60px'}} fontWeight='700'>
+                                                    {event.date.toLocaleDateString('default', {day: "2-digit", timeZone: 'UTC'})}
+                                                </Text>
+                                            </Flex>
+                                            <Flex w='min(150px, 10%)'></Flex>
 
-                                        <Flex
-                                        id='event-info'
-                                        flexDirection='column'
-                                        alignItems='flex-start'
-                                        marginY='15px'
-                                        >
-                                            <Text textAlign='left' lineHeight='30px' fontSize={{base: '30px', sm:'40px'}}>{event.title}</Text> 
+                                            <Flex
+                                            id='event-info'
+                                            flexDirection='column'
+                                            alignItems='flex-start'
+                                            marginY='15px'
+                                            >
+                                                <Text textAlign='left' lineHeight='30px' fontSize={{base: '30px', sm:'40px'}}>{event.title}</Text> 
+                                                <Spacer/>
+                                                <Text textAlign='left' fontSize={{base: '18px', sm: '24px'}}>{event.location}</Text>
+                                            </Flex>
                                             <Spacer/>
-                                            <Text textAlign='left' fontSize={{base: '18px', sm: '24px'}}>{event.location}</Text>
+                                            <Flex
+                                            alignItems='center'>
+                                                <Image
+                                                h='50%'
+                                                src={yellowRightArrow}
+                                                />
+                                            </Flex>
                                         </Flex>
-                                        <Spacer/>
-                                        <Flex
-                                        alignItems='center'>
-                                            <Image
-                                            h='50%'
-                                            src={yellowRightArrow}
-                                            />
-                                        </Flex>
-                                    </Flex>
-                                </LinkBox>
-                            )}
-                        </Flex>
-                    )}
-                </Flex>    
-            </Flex>
-        </Box>
+                                    </LinkBox>
+                                )}
+                            </Flex>
+                        )}
+                    </Flex>    
+                </Flex>
+            </Box>
+        </Linkify>
     )
 }
 
--- src/pages/GetInvolved.tsx
+++ src/pages/GetInvolved.tsx
@@ -1,4 +1,5 @@
-import { SimpleGrid } from '@chakra-ui/react';
+import blogheader from '../assets/blogheader.jpg';
+import { Box, Image, Text, SimpleGrid } from '@chakra-ui/react';
 import GetInvolvedCard from '../components/GetInvolvedCard';
 import { GetInvolvedInfo } from '../components/GetInvolvedCard';
 import { getInvolvedDonate, getInvolvedEvents, getInvolvedInternships, getInvolvedMmerch } from '../components/constants/constants';
@@ -40,11 +41,20 @@ function GetInvolved() {
   });
 
   return (
+    <Box w='100%'>
+      <>
+            <Box h='100px' position='relative'>
+            <Image position='absolute' src={blogheader} h='100px' w='100%' objectFit='cover' zIndex='0'/>
+            <Box position='relative' h='100px' w='100%' bgGradient='linear(to-r, rgba(47, 71, 53, 0.8), rgba(7, 19, 25, 0))' />
+            <Text position='absolute' left='0' right='0' top='50px' bottom='0' m='auto' w='100%' h='100px' textColor='white' fontSize='3xl' fontWeight='bold'>Get Involved</Text>
+            </Box>
       <SimpleGrid px={['10px', null, null, '60px', '150px']} columns={[1, null, 2]} spacing={5} my={12}>
         {cards.map((card) => 
           <GetInvolvedCard {...card} key={card.header}></GetInvolvedCard>
         )}
       </SimpleGrid>
+      </>
+    </Box>
   );
 }
 
