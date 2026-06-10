7a9cd3f6dcd57095c23a534bf61954669e685311
KOAJURGENSEN <koaj@colostate.edu>
2026-06-09 18:28:06 -0600

    Added k3ys to supabaseclient.ts

--- src/lib/supabaseclient.ts
+++ src/lib/supabaseclient.ts
@@ -1,8 +1,8 @@
 import { createClient, SupabaseClient } from '@supabase/supabase-js'
 
 
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
-const supabasePublishableKey = import.meta.env.VITE_SUPABASE_PUBLISHABLE_KEY
+const supabaseUrl = 'https://wtwvdfrngbsicwvythxs.supabase.co'
+const supabasePublishableKey = 'sb_publishable_f51_JUTRBLUv4ss-ezHcgA_YS_tSsRL'
 
 export const supabase: SupabaseClient = createClient(supabaseUrl, supabasePublishableKey, {
   auth: {
