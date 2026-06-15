864fe8970145c8ff930665e207336321221dcc2c
KOAJURGENSEN <koaj@colostate.edu>
2026-06-14 20:40:06 -0600

    Fixed style issue in supabaseClient.ts

--- src/lib/supabaseClient.ts
+++ src/lib/supabaseClient.ts
@@ -1,10 +1,10 @@
 import { createClient, SupabaseClient } from '@supabase/supabase-js'
 
 
-const supabaseUrl = 'https://wtwvdfrngbsicwvythxs.supabase.co'
-const supabasePublishableKey = 'sb_publishable_f51_JUTRBLUv4ss-ezHcgA_YS_tSsRL'
+const SUPABASE_URL = 'https://wtwvdfrngbsicwvythxs.supabase.co'
+const SUPABASE_PUBLISHABLE_KEY = 'sb_publishable_f51_JUTRBLUv4ss-ezHcgA_YS_tSsRL'
 
-export const supabase: SupabaseClient = createClient(supabaseUrl, supabasePublishableKey, {
+export const supabase: SupabaseClient = createClient(SUPABASE_URL, SUPABASE_PUBLISHABLE_KEY, {
   auth: {
     autoRefreshToken: true,
     persistSession: true,
