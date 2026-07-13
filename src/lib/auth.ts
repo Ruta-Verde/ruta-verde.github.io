dc1482934d1fa33a87291888949f8a3ecd83825a
KOAJURGENSEN <koaj@colostate.edu>
2026-07-12 18:43:52 -0600

    oauth redirect using PCKE flow

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -4,7 +4,7 @@ export const signInWithGoogle = async (): Promise<void> => {
   await supabase.auth.signInWithOAuth({
     provider: 'google',
     options: {
-      redirectTo: `${window.location.origin}/auth/callback`,
+      redirectTo: 'https://rutaverde.org/#/auth/callback'
     },
   })
 }
